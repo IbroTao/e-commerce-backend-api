25c34756b71ff42aecab27f18c0ffbf88eb626c3
IbroTao <ibrahimtaofeek005@gmail.com>
2023-11-14 02:10:28 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const server = express();
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 4000;
+
+server.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
